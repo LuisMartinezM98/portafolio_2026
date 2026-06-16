66bcdc51c1537c3a9cc9eb7b8b6c245aa6e83277
LuisMartinezM98 <104807270+LuisMartinezM98@users.noreply.github.com>
2026-06-15 23:37:04 -0600

    refactor: replace Material Symbols with a custom Icon component and implement self-hosted fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 // @ts-check
-import { defineConfig, passthroughImageService } from 'astro/config';
+import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  image: {
-    service: passthroughImageService()
-  },
   vite: {
     plugins: [tailwindcss()]
   }
